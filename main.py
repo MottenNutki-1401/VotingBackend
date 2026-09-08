d3873fb3219e1ceb0095f28db44c7f9add79ecf3
MottenNutki-1401 <202310763@gordoncollege.edu.ph>
2026-09-09 04:05:09 +0800

    test

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
        " https://frontend-cc-refz-m61a38d13-comettrail-s-projects.vercel.app/"
+       #test
     ],
     allow_credentials=True,
     allow_methods=["*"],
