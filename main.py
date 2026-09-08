b9c83f19e4bbff6d9ddcb5c5c5e49f2fd695aa12
MottenNutki-1401 <202310763@gordoncollege.edu.ph>
2026-09-09 04:46:26 +0800

    new cors

--- main.py
+++ main.py
@@ -25,15 +25,13 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-       " https://frontend-cc-refz-m61a38d13-comettrail-s-projects.vercel.app/"
         "https://frontend-cc-refz.vercel.app",
-       #testdddd
+        "https://frontend-cc-refz-m61a38d13-comettrail-s-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 
 @app.get("/")
